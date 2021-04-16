536af02a3021047c7dd068ae4d71a3856843a79b
janevski.stefan <38108655+janevski400@users.noreply.github.com>
2021-04-16 19:29:29 +0200

    Update labMIPS1_201117.asm

--- labMIPS1_201117.asm
+++ labMIPS1_201117.asm
@@ -18,8 +18,11 @@
 		addi $t0,$t0,1				#zgolemuvanje na brojachot za 1
 		j loop					#nazad vo loop
 	end:	
+		add $a0, $s1, $zero			#podesuvanje na argumenti za funkcijata
 		jal proizvodNeparni			#povikuvanje na procedura	
-		la $a0,($t6)				#zapishuvanje na adresata na rezultatot od mnozhenjeto na site neparni broevi vo $a0 koja kje bide iskoristena od syscall
+		sw $v0, ($s0)				#zachuvaj go rezultatot od mnozhenjeto na site neparni broevi vo memoriska lokacija $a0 (odnosno $s0)
+		
+		la $a0,($v0)				#zapishuvanje na adresata na rezultatot od mnozhenjeto na site neparni broevi vo $a0 koja kje bide iskoristena od syscall
 		li $v0, 1				#syscall za pechatenje na integer bara $v0 = 5
 		syscall					#pechatenje na rezultatot od mnozhenje na site neparni broevi
 		add $s3, $zero, $t6			#zachuvuvanje na rezultatot vo $s3
@@ -37,13 +40,14 @@
 	syscall						#kraj na programa
 
 	proizvodNeparni:
+		#$a1 - broj na elementi vo nizata
 		li $t0,0				#setiranje na $t0 na 0
 		li $t2,0				#seturanje na $t1 na 0
 		li $t3,0				#seturanje na $t1 na 0
 		li $t4,2				#setiranje na $t4 na 2 za da mozheme da delime so 2
 		li $t5,0				#setiranje na $t5 na 0 
 		li $t6,1				#setiranje na $t6 na 1 za vo sluchaj da nema neparni elementi da se ispechati 1
-		loop2:	beq $t0,$s1,end2		#uslov
+		loop2:	beq $t0,$a0,end2		#uslov
 			sll $t2, $t0, 2			#dobivanje na offset
 			lw $t3,Array($t2)		#dobivanje na i-ti element od niza Array
 			div $t3, $t4			#delenje na i-tiot element od nizata so 2
@@ -55,7 +59,7 @@
 			addi $t0, $t0, 1		#zgolemuvanje na brojachot za 1
 			j loop2				#vrati se do loop2
 		end2:
-		sw $t6, ($s0)				#zachuvaj go rezultatot od mnozhenjeto na site neparni broevi vo memoriska lokacija $s0
+		add $v0,$t6,$zero			#vrati kako rezultat od procedura $t6 (odnosno rezultat od mnozhenje)
 		jr $ra					#vrati se vo glavna programa
 		
 		
